2f5b601d20fd38bba95e9244238b950af1734d6b
Ken Erwin <ken@devopslibrary.com>
2020-03-29 23:59:21 -0400

    known working

--- backend/src/repoSync/repoSync.service.ts
+++ backend/src/repoSync/repoSync.service.ts
@@ -8,30 +8,94 @@ export class RepoSyncService {
 
   // Clones/Pulls/Updates repository locally
   async sync(cloneURL: string, clonePath: string, token: string) {
-    cloneURL = cloneURL
-      .toLowerCase()
-      .replace('https://github.com/', `https://${token}@github.com/`);
+    cloneURL = cloneURL.replace(
+      'https://github.com/',
+      `https://${token}@github.com/`,
+    );
 
     // Clone, unless already pulled down, then update.
-    if (!fs.existsSync(clonePath.toLowerCase())) {
+    if (!fs.existsSync(clonePath)) {
       await git()
         .silent(false)
-        .clone(cloneURL.toLowerCase(), clonePath.toLowerCase())
-        .catch((err) => console.error('failed: ', err))
-        .then(() => {
-          this.logger.log(
-            'Cloned ' + clonePath.toLowerCase() + ' successfully.',
-          );
-        });
+        .clone(cloneURL, clonePath)
+        .catch((err) => console.error('failed: ', err));
+      this.logger.log('Cloned ' + clonePath + ' successfully.');
     } else {
-      // await git(clonePath).removeRemote('origin');
-      // await git(clonePath).addRemote('origin', cloneURL.toLowerCase());
-      await git(clonePath)
-        .pull()
-        .then(() => {
-          this.logger.log('Pulled latest for ' + clonePath.toLowerCase());
-        });
+      await git(clonePath).pull();
     }
   }
 }
-// .pull()
+// import { Injectable, Logger } from '@nestjs/common';
+// const fs = require('fs');
+// const git = require('simple-git/promise');
+
+// @Injectable()
+// export class RepoSyncService {
+//   private readonly logger = new Logger(RepoSyncService.name);
+
+//   // Clones/Pulls/Updates repository locally
+//   async sync(cloneURL: string, clonePath: string, token: string) {
+//     cloneURL = cloneURL
+//       .toLowerCase()
+//       .replace('https://github.com/', `https://${token}@github.com/`);
+
+//     // Clone, unless already pulled down, then update.
+//     if (!fs.existsSync(clonePath.toLowerCase())) {
+//       await git()
+//         .silent(false)
+//         .clone(cloneURL.toLowerCase(), clonePath.toLowerCase())
+//         .catch((err) => console.error('failed: ', err))
+//         .then(() => {
+//           this.logger.log(
+//             'Cloned ' + clonePath.toLowerCase() + ' successfully.',
+//           );
+//         });
+//     } else {
+//       // await git(clonePath).removeRemote('origin');
+//       // await git(clonePath).addRemote('origin', cloneURL.toLowerCase());
+//       await git(clonePath)
+//         .pull()
+//         .then(() => {
+//           this.logger.log('Pulled latest for ' + clonePath.toLowerCase());
+//         });
+//     }
+//   }
+// }
+// // .pull()
+// // import { Injectable, Logger } from '@nestjs/common';
+// // const fs = require('fs');
+// // const git = require('simple-git/promise');
+
+// // @Injectable()
+// // export class RepoSyncService {
+// //   private readonly logger = new Logger(RepoSyncService.name);
+
+// //   // Clones/Pulls/Updates repository locally
+// //   async sync(cloneURL: string, clonePath: string, token: string) {
+// //     cloneURL = cloneURL
+// //       .toLowerCase()
+// //       .replace('https://github.com/', `https://${token}@github.com/`);
+
+// //     // Clone, unless already pulled down, then update.
+// //     if (!fs.existsSync(clonePath.toLowerCase())) {
+// //       await git()
+// //         .silent(false)
+// //         .clone(cloneURL.toLowerCase(), clonePath.toLowerCase())
+// //         .catch((err) => console.error('failed: ', err))
+// //         .then(() => {
+// //           this.logger.log(
+// //             'Cloned ' + clonePath.toLowerCase() + ' successfully.',
+// //           );
+// //         });
+// //     } else {
+// //       await git(clonePath).removeRemote('origin');
+// //       await git(clonePath).addRemote('origin', cloneURL.toLowerCase());
+// //       await git(clonePath)
+// //         .pull()
+// //         .then(() => {
+// //           this.logger.log('Pulled latest for ' + clonePath.toLowerCase());
+// //         });
+// //     }
+// //   }
+// // }
+// // // .pull()

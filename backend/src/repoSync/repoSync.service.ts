2b57be6506d3b1db378c8d261ee38ce9d6f24588
Ken Erwin <ken@devopslibrary.com>
2020-03-30 00:19:02 -0400

    completed!

--- backend/src/repoSync/repoSync.service.ts
+++ backend/src/repoSync/repoSync.service.ts
@@ -8,6 +8,9 @@ export class RepoSyncService {
 
   // Clones/Pulls/Updates repository locally
   async sync(cloneURL: string, clonePath: string, token: string) {
+    cloneURL = cloneURL.toLowerCase();
+    clonePath = clonePath.toLowerCase();
+
     cloneURL = cloneURL.replace(
       'https://github.com/',
       `https://${token}@github.com/`,
@@ -21,64 +24,13 @@ export class RepoSyncService {
         .catch((err) => console.error('failed: ', err));
       this.logger.log('Cloned ' + clonePath + ' successfully.');
     } else {
-      await git(clonePath).pull();
+      await git(clonePath).removeRemote('origin');
+      await git(clonePath).addRemote('origin', cloneURL.toLowerCase());
+      await git(clonePath)
+        .pull()
+        .then(() => {
+          this.logger.log('Pulled latest for ' + clonePath.toLowerCase());
+        });
     }
   }
 }
-//   async sync(cloneURL: string, clonePath: string, token: string) {
-//     cloneURL = cloneURL
-//       .toLowerCase()
-//       .replace('https://github.com/', `https://${token}@github.com/`);
-
-//     // Clone, unless already pulled down, then update.
-//     if (!fs.existsSync(clonePath.toLowerCase())) {
-//       await git()
-//         .silent(false)
-//         .clone(cloneURL.toLowerCase(), clonePath.toLowerCase())
-//         .catch((err) => console.error('failed: ', err))
-//         .then(() => {
-//           this.logger.log(
-//             'Cloned ' + clonePath.toLowerCase() + ' successfully.',
-//           );
-//         });
-//     } else {
-//       // await git(clonePath).removeRemote('origin');
-//       // await git(clonePath).addRemote('origin', cloneURL.toLowerCase());
-//       await git(clonePath)
-//         .pull()
-//         .then(() => {
-//           this.logger.log('Pulled latest for ' + clonePath.toLowerCase());
-//         });
-//     }
-//   }
-// }
-// // .pull()
-// // import { Injectable, Logger } from '@nestjs/common';
-// // const fs = require('fs');
-// // const git = require('simple-git/promise');
-
-// // @Injectable()
-// // export class RepoSyncService {
-// //   private readonly logger = new Logger(RepoSyncService.name);
-
-// //   // Clones/Pulls/Updates repository locally
-// //   async sync(cloneURL: string, clonePath: string, token: string) {
-// //     cloneURL = cloneURL
-// //       .toLowerCase()
-// //       .replace('https://github.com/', `https://${token}@github.com/`);
-
-// //     // Clone, unless already pulled down, then update.
-// //     if (!fs.existsSync(clonePath.toLowerCase())) {
-// //       await git()
-// //         .silent(false)
-// //         .clone(cloneURL.toLowerCase(), clonePath.toLowerCase())
-// //         .catch((err) => console.error('failed: ', err))
-// //         .then(() => {
-// //           this.logger.log(
-// //             'Cloned ' + clonePath.toLowerCase() + ' successfully.',
-// //           );
-// //         });
-
-// //   }
-// // }
-// // // .pull()
